58a41126f3bafe66438076e1c565f0aae2befcbd
Lord-Jony <62217008+Jony-07@users.noreply.github.com>
2022-02-16 22:04:22 -0600

    Commit
    s

---
+++ views/inicio/main.php
@@ -0,0 +1,73 @@
+<div class="row mx-5 mt-5">
+        <div class="col ml-5">
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+                <div class="col">
+                    <div class="card text-white bg-dark mb-3">
+                        <h3 class="card-title text-center">Zona de Redes</h3>
+                        <img src="" class="card-img-top" alt="">
+                        <div class="card-body border border-success">
+                            <h4 align="center">Cantidad de Productos</h4>
+                            <p class="card-text text-center"></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card text-white bg-dark mb-3">
+                        <h3 class="card-title text-center">Zona de Laptops</h3>
+                        <img src="" class="card-img-top">
+                        <div class="card-body border border-success">
+                            <h4 align="center">Cantidad de Productos</h4>
+                            <p class="card-text text-center"></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card text-white bg-dark mb-3">
+                        <h3 class="card-title text-center">Zona de PC</h3>
+                        <img src="" class="card-img-top">
+                        <div class="card-body border border-success">
+                            <h4 align="center">Cantidad de Productos</h4>
+                            <p class="card-text text-center"></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<br>
+<div class="d-grid gap-3 mt-3 mx-5">
+      
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+                    aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                    aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                    aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="assets/img/img.png" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="assets/img/img.png" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="assets/img/img.png" class="d-block w-100" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+                data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+                data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+</div>
+</body>
+        </html>
